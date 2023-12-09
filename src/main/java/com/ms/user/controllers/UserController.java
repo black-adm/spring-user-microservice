8ffc3823a355fce17fdb90f90898ca080ce229b1
black-adm <mattheuscontato17@gmail.com>
2023-12-09 09:53:21 -0300

    feat: validation user exists

--- src/main/java/com/ms/user/controllers/UserController.java
+++ src/main/java/com/ms/user/controllers/UserController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.ms.user.dtos.UserRecordDto;
 import com.ms.user.models.UserModel;
+import com.ms.user.repositories.UserRepository;
 import com.ms.user.services.UserService;
 
 import jakarta.validation.Valid;
@@ -18,15 +19,23 @@ import org.springframework.web.bind.annotation.RequestBody;
 public class UserController {
     
     final UserService userService;
+    final UserRepository userRepository;
 
-    public UserController(UserService userService) {
+    public UserController(UserService userService, UserRepository userRepository) {
         this.userService = userService;
+        this.userRepository = userRepository;
     }
 
     @PostMapping("/users")
     public ResponseEntity<UserModel> create(@RequestBody @Valid UserRecordDto userRecordDto) {        
         var userModel = new UserModel();
-        BeanUtils.copyProperties(userRecordDto, userModel);
+        BeanUtils.copyProperties(userRecordDto, userModel); 
+
+        var userExists = userRepository.findByEmail(userModel.getEmail());
+
+        if (userExists != null) {
+            ResponseEntity.status(HttpStatus.CONFLICT).build();
+        }
         
         return ResponseEntity.status(HttpStatus.CREATED).body(userService.save(userModel));
     }
--- src/main/java/com/ms/user/repositories/UserRepository.java
+++ src/main/java/com/ms/user/repositories/UserRepository.java
@@ -5,6 +5,9 @@ import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.ms.user.models.UserModel;
+import java.util.List;
+
 
 public interface UserRepository extends JpaRepository<UserModel, UUID> {
+    List<UserModel> findByEmail(String email);
 }
